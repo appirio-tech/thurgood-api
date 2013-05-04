aa1010049ed9e1957680f2ae8d6a8ad3d7b2761a
Jeff Douglas <jeff@jeffdouglas.com>
2013-05-04 06:17:11 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.12'
+
+gem 'rocket_pants', '~> 1.0'
+gem 'pg'
+gem 'unicorn'
+
+group :development, :test do
+	gem 'rspec-rails', '~> 2.6'
+	gem 'sextant'
+	gem 'rake'
+end
+
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'meta_request', '0.2.1' # see https://github.com/dejan/rails_panel
+end
+
+gem 'debugger'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThurgoodApi::Application.config.secret_token = '9b755dd531001f5f36b9291559db94165f5fcb140076ad5f36fd5619cda666442ebb5168fe49483ba0b79cf35f8813690387aa3d65a8a2383049e43b20be3133'
